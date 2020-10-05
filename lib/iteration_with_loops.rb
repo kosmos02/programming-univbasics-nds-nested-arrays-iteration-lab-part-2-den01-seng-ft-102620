bc187fc64aac138a8d284cbc746860e79cceac73
Alexander Gabriel <alexandrgabe@gmail.com>
2020-10-05 16:41:13 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def find_min_in_nested_arrays(src)
   
   while row_index < src.count do
     element_index = 0 
-    lowest_temp = ""
+    lowest_temp = 
     
     while element_index < src[row_index].count do
       
